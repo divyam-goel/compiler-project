61422a233d345fc69ec12be50d49f79c709635e2
Divyam Goel <divyamgoel19@gmail.com>
2020-02-19 15:54:11 +0530

    Added lexeme union and symbol struct

--- src/lexer.h
+++ src/lexer.h
@@ -7,16 +7,6 @@
 #define BUFFER_SIZE 512
 #define CHAR_BUFFER_SIZE 10
 
-void removeComments(char *testcaseFile, char *cleanFile);
-int getNumBytes();
-char getChar(int index);
-char getNextChar(FILE *fp);
-bool isBufferEnd();
-void getStream(FILE * fp);
-
-int getNextToken();
-
-
 struct twinBuffer {
 	char buffer_1[CHAR_BUFFER_SIZE];
 	char buffer_2[CHAR_BUFFER_SIZE];
@@ -31,20 +21,43 @@ struct twinBuffer {
 	bool flag_retract;
 };
 
-
-enum TOKENS {
-	IDENTIFIER,
-	NUM, RNUM, // numbers
-	PLUS, MINUS, MUL, DIV, // arithmetic
-	LT, LE, GE, GT, EQ, NE, // relational
-	DEF, DRIVERDEF, ENDDEF, DRIVERENDDEF, //  module
-	SQBO, SQBC, BO, BC, // brackets
+enum terminal {
+	IDENTIFIER, // 1
+	NUM, RNUM, // numbers // 2
+	PLUS, MINUS, MUL, DIV, // arithmetic // 4
+	LT, LE, GE, GT, EQ, NE, // relational // 6
+	DEF, DRIVERDEF, ENDDEF, DRIVERENDDEF, //  module // 4
+	SQBO, SQBC, BO, BC, // brackets // 4
 	// others
-	RANGEOP,
+	RANGEOP, // index - 21
 	COLON,
 	ASSIGNOP,
 	SEMICOL,
 	COMMA
 };
+
+
+union lexeme {
+	char str[21];
+	int num;
+	float rnum;
+};
+
+
+struct symbol {
+	enum terminal token;
+	union lexeme lexeme;
+	unsigned int line_no;
+};
+
+void removeComments(char *testcaseFile, char *cleanFile);
+int getNumBytes();
+char getChar(int index);
+char getNextChar(FILE *fp);
+bool isBufferEnd();
+void getStream(FILE * fp);
+
+struct symbol getNextToken();
+
 
 #endif
