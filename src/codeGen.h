aaad94dad79665b698767a1fa694e86b622bada3
lesasi <nevinusa@gmail.com>
2020-04-14 20:11:30 +0530

    codeGen: Added function prototypes to codeGen.h

--- src/codeGen.h
+++ src/codeGen.h
@@ -1,4 +1,24 @@
 #include "intCode.h"
 #define MAX_SIZE_INSTR 100
-void cg_ADD_SUB(ICInstr *ic_instr);
-void cgSTORE(ICInstr *ic_instr);
+void cgICAddr(char *instr_list, char *addr, ICAddr *ic_addr);
+void addLabel(char *instr_list, char *label);
+void loadConstReg(char *instr_list, char *reg1, int num);
+void instrOneOperand(char *instr_list, char *op, char *reg1); 
+void instrTwoOperand(char *instr_list, char *op, char *reg1, char *reg2);
+void cgLoadINT(char *instr_list, char *reg, ICAddr *ic_addr);
+void cgLoadReal(char *instr_list, char *reg, ICAddr *ic_addr);
+void cgStoreINT(char *instr_list, char *reg, ICAddr *ic_addr);
+void cgStoreREAL(char *instr_list, char *reg, ICAddr *ic_addr);
+void cgADD_SUB_INT(ICInstr *ic_instr);
+void cgMUL_INT(ICInstr *ic_instr);
+void cgDIV_INT(ICInstr *ic_instr);
+void cgMUL_REAL(ICInstr *ic_instr);
+void cgDIV_REAL(ICInstr *ic_instr);
+void cgINC(ICInstr *ic_instr);
+void cgDEV(ICInstr *ic_instr);
+void cgSTORE_INT(ICInstr *ic_instr);
+void cgRelationalOp(ICInstr *ic_instr);
+void cgLogicalOp(ICInstr *ic_instr);
+void cgPLUS_MINUS(ICInstr *ic_instr);
+void generateASMCode(ICInstr *ic_instr);
+
