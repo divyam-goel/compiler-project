divyam-goel/compiler-project

commit 2071639b6b52352ceabca372b38850466e0714db

Added support for nodule reuse
test case c10 working

src/codeGen.c
@@ -1,4 +1,5 @@
 #include "codeGen.h"
+#include <math.h>
 
 #define OFFSET 8
 
@@ -23,6 +24,8 @@ char instr_jmp[] = "jmp";
 char instr_je[] = "je";
 char instr_jne[] = "jne";
 char instr_cmp[] = "cmp";
+char instr_call[] = "call";
+char instr_ret[] = "ret";
 char instr_push[] = "push";
 char instr_pop[] = "pop";
 char instr_inc[] = "inc";
@@ -362,7 +365,10 @@ void cgICAddr(char *instr_list, char *addr, ICAddr *ic_addr) {
     }
     else {
       symbol_entry = (struct VariableEntry *)ic_addr->value.symbol;
-      sprintf(addr, "[rbp - %d]", symbol_entry->mem_offset + OFFSET);
+      if (symbol_entry->mem_offset >= 0)
+        sprintf(addr, "[rbp - %d]", symbol_entry->mem_offset + OFFSET);
+      else
+        sprintf(addr, "[rbp + %d]", abs(symbol_entry->mem_offset) + OFFSET);
     }
     break;
   case ARRAY:
@@ -406,6 +412,7 @@ void cgICAddr(char *instr_list, char *addr, ICAddr *ic_addr) {
     
     sprintf(addr, "[%s + %s * %d]", reg_rdx, reg_rbx, data_size);
     break;
+
   default:
     sprintf(addr, "NULL");
     break;
@@ -414,12 +421,10 @@ void cgICAddr(char *instr_list, char *addr, ICAddr *ic_addr) {
 
 
 void cgMoveFromMemToReg(char *instr_list, char *reg, ICAddr *ic_addr) { 
-  char op[50];
   char addr[50];
   
-  strcpy(op, "mov");
   cgICAddr(instr_list, addr, ic_addr);  
-  instrTwoOperand(instr_list, op, reg, addr);
+  instrTwoOperand(instr_list, instr_mov, reg, addr);
 }
 
 
@@ -1207,6 +1212,123 @@ void cgArrayOutOfBoundsError(ICInstr *ic_instr) {
 }
 
 
+void cgCallFunction(ICInstr *ic_instr) {
+  char instr_list[MAX_SIZE_INSTR] = "";
+  char addr[50];
+
+  cgICAddr(instr_list, addr, &(ic_instr->addr1));
+  instrOneOperand(instr_list, instr_call, addr);
+
+  writeToOutputFile(instr_list);
+}
+
+
+void cgEnterFunction(ICInstr *ic_instr) {
+  char instr_list[MAX_SIZE_INSTR] = "";
+  char addr[50];
+  
+  /*
+    push the current value of base register to preserve
+    the base address of activation record of caller
+  */
+  instrOneOperand(instr_list, instr_push, reg_rbp);
+
+  /* update the base register with base address of callee */
+  instrTwoOperand(instr_list, instr_mov, reg_rbp, reg_rsp);
+  
+  /*
+    push the activation record of the callee by subtracting
+    the size of the activation record from base pointer
+  */
+  cgICAddr(instr_list, addr, &(ic_instr->addr1));
+  instrTwoOperand(instr_list, instr_sub, reg_rsp, addr);
+
+  writeToOutputFile(instr_list);
+}
+
+
+void cgReturnFromFunction(ICInstr *ic_instr) {
+  char instr_list[MAX_SIZE_INSTR] = "";
+
+  /*
+    discard the activation record of callee, and
+    all the dynamic arrays (if any)
+  */
+  instrTwoOperand(instr_list, instr_mov, reg_rsp, reg_rbp);
+
+  /*
+    restore old value of base register i.e.
+    base of activation record of caller
+  */
+  instrOneOperand(instr_list, instr_pop, reg_rbp);
+
+  /* return to the caller function */
+  strcat(instr_list, "ret\n");
+
+  writeToOutputFile(instr_list);
+}
+
+
+void cgPushToStack(ICInstr *ic_instr) {
+  char instr_list[MAX_SIZE_INSTR] = "";
+  
+  /* load the value to be pushed in register RAX from memory */
+  cgMoveFromMemToReg(instr_list, reg_rax, &(ic_instr->addr1));
+  
+  /* push the value in register RAX on the stack */
+  instrOneOperand(instr_list, instr_push, reg_rax);
+
+  writeToOutputFile(instr_list);
+}
+
+
+void cgPopFromStack(ICInstr *ic_instr) {
+  char instr_list[MAX_SIZE_INSTR] = "";
+  
+  /* pop the value from top of stack and store in register RAX */
+  instrOneOperand(instr_list, instr_pop, reg_rax);
+
+  /* store the value in register RAX in memory */
+  cgMoveFromRegToMem(instr_list, reg_rax, &(ic_instr->addr1));
+
+  writeToOutputFile(instr_list);
+}
+
+
+void cgShadowPushStack(ICInstr *ic_instr) {
+  char instr_list[MAX_SIZE_INSTR] = "";
+  char addr[50];
+
+  /* get the value in address */
+  cgICAddr(instr_list, addr, &(ic_instr->addr1));
+
+  /*
+    add the above value from stack pointer to
+    increase space on stack
+  */
+  instrTwoOperand(instr_list, instr_sub, reg_rsp, addr);
+
+  writeToOutputFile(instr_list);
+}
+
+
+void cgShadowPopStack(ICInstr *ic_instr) {
+  char instr_list[MAX_SIZE_INSTR] = "";
+  char addr[50];
+
+  /* get the value in address */
+  cgICAddr(instr_list, addr, &(ic_instr->addr1));
+
+  /*
+    add the above value from stack pointer to
+    decrease space on stack
+  */
+  instrTwoOperand(instr_list, instr_add, reg_rsp, addr);
+
+  writeToOutputFile(instr_list);
+}
+
+
 /* Functions for interfacing with ASM Code Generation */
 
 void generateASMInstruction(ICInstr *ic_instr) {
@@ -1247,14 +1369,6 @@ void generateASMInstruction(ICInstr *ic_instr) {
       cgDivisionInteger(ic_instr);
       break;
 
-    /* arithmetic & term op - real */
-    // case icADD_REAL:
-    // case icSUB_REAL:
-    // case icMUL_REAL:
-    // case icDIV_REAL:
-    //   cgArithmeticOpReal(ic_instr);
-    //   break;
-
     /* increment & decrement */
     case icINC:
       cgIncrement(ic_instr);
@@ -1286,9 +1400,33 @@ void generateASMInstruction(ICInstr *ic_instr) {
       cgJumpConditional(ic_instr);
       break;
 
-    // case icCALL:
-    //   printf("CALL\t\t\t");
-    //   break;
+    case icCALL:
+      cgCallFunction(ic_instr);
+      break;
+    
+    case icENTER:
+      cgEnterFunction(ic_instr);
+      break;
+    
+    case icRET:
+      cgReturnFromFunction(ic_instr);
+      break;
+    
+    case icPUSH:
+      cgPushToStack(ic_instr);
+      break;
+    
+    case icPOP:
+      cgPopFromStack(ic_instr);
+      break;
+    
+    case icPUSH_SHADOW:
+      cgShadowPushStack(ic_instr);
+      break;
+    
+    case icPOP_SHADOW:
+      cgShadowPopStack(ic_instr);
+      break;
 
     case icARRAY:
       cgDynamicArrayAllocation(ic_instr);

src/st.c
@@ -234,6 +234,8 @@ stCreateSymbolTableValueForModule (char *name, int dec_line_no, int def_line_no,
   new_value.module.called = false;
   new_value.module.inputplist = ipl;
   new_value.module.outputplist = opl;
+  new_value.module.activation_record_size = (8 + 8);
+  new_value.module.io_record_size = 0;
   return new_value;
 }
 
@@ -281,9 +283,10 @@ stCreateSymbolTableValueForVariable (struct LeafNode *varnode, struct Attribute 
       module->io_record_size -= 8;
       memoffset = module->io_record_size;
     }
-  else
+  else {
     memoffset = module->activation_record_size;
-  module->activation_record_size += 8;
+    module->activation_record_size += 8;
+  }
 
   if (dtnode->type == ARRAY_TYPE_NODE)
     {
@@ -766,9 +769,8 @@ stHandleModuleReuseStatement (struct ModuleReuseStmtNode *mr_stmt, struct Symbol
   else
     {
       module->called = true;
-      module->activation_record_size = (8 + 8);
-      module->io_record_size = 0;
-    }
+  // module->activation_record_size = (8 + 8);
+  // module->io_record_size = 0;
   mr_stmt->ptr2->scope = global_symbol_table;
   while (inputs_ll != NULL)
     {
@@ -1092,17 +1094,17 @@ getDatatypeSize (enum terminal datatype)
     {
       case (NUM):
       case (INTEGER):
-        // return DT_INTEGER_SIZE;
-        return 2;
+        return DT_INTEGER_SIZE;
+        // return 2;
       case (RNUM):
       case (REAL):
-        // return DT_REAL_SIZE;
-        return 4;
+        return DT_REAL_SIZE;
+        // return 4;
       case (TRUE_):
       case (FALSE_):
       case (BOOLEAN_):
-        // return DT_BOOL_SIZE;
-        return 1;
+        return DT_BOOL_SIZE;
+        // return 1;
       default:
         fprintf(stderr, invalid_datatype_error_message, terminalStringRepresentations[datatype]);
         exit(EXIT_FAILURE);

src/intCode.c
@@ -547,6 +547,114 @@ void icAssignmentStatement(struct AssignStmtNode *assignment) {
 }
 
 
+void icModuleReuseStatement(struct ModuleReuseStmtNode *module_reuse) {
+  ICInstr *ic_instr;
+  ICAddr ic_addr;
+  struct IdListNode *id_list_node;
+  struct IdListNode *id_list_node_prev, *id_list_node_curr;
+  int size;
+
+  /* push the input parameters on the stack */
+  id_list_node = module_reuse->ptr3;
+  while (id_list_node != NULL) {
+    icLeaf(id_list_node->ptr1);
+    ic_instr = newICInstruction();
+    ic_instr->addr1 = id_list_node->ptr1->addr;
+    ic_instr->op = icPUSH;
+    global_ic_instr->next = ic_instr;
+    global_ic_instr = ic_instr;
+
+    id_list_node = id_list_node->ptr2;
+  }
+
+  /*
+    calculate the total size of output parameters and
+    make space for them on the stack using shadow instruction
+  */
+  size = 0;
+  id_list_node = module_reuse->ptr1;
+  while (id_list_node != NULL) {
+    size += 8;
+
+    id_list_node = id_list_node->ptr2;
+  }
+
+  ic_addr.type = INTEGER;
+  ic_addr.value.num = size;
+  ic_addr.is_label = false;
+
+  ic_instr = newICInstruction();
+  ic_instr->addr1 = ic_addr;
+  ic_instr->op = icPUSH_SHADOW;
+  global_ic_instr->next = ic_instr;
+  global_ic_instr = ic_instr;
+
+  /* populate address for function label */
+  ic_addr.type = IDENTIFIER;
+  ic_addr.value.symbol = module_reuse->ptr2->value.entry;
+  ic_addr.is_label = true;
+
+  /* call the function using above address */
+  ic_instr = newICInstruction();
+  ic_instr->addr1 = ic_addr;
+  ic_instr->op = icCALL;
+  global_ic_instr->next = ic_instr;
+  global_ic_instr = ic_instr;
+
+  /* reverse the output parameter list to pop from stack */
+  id_list_node_prev = NULL;
+  id_list_node_curr = module_reuse->ptr1;
+  while (id_list_node_curr != NULL) {
+    id_list_node = id_list_node_curr->ptr2;
+    id_list_node_curr->ptr2 = id_list_node_prev;
+    id_list_node_prev = id_list_node_curr;
+    id_list_node_curr = id_list_node;
+  }
+
+  /* pop the output parameters from the stack */
+  id_list_node = id_list_node_prev;
+  while (id_list_node != NULL) {
+    icLeaf(id_list_node->ptr1);
+    ic_instr = newICInstruction();
+    ic_instr->addr1 = id_list_node->ptr1->addr;
+    ic_instr->op = icPOP;
+    global_ic_instr->next = ic_instr;
+    global_ic_instr = ic_instr;
+
+    id_list_node = id_list_node->ptr2;
+  }
+
+  /* reverse the output parameter list to restore original order */
+  id_list_node_curr = id_list_node_prev;
+  id_list_node_prev = NULL;
+  while (id_list_node_curr != NULL) {
+    id_list_node = id_list_node_curr->ptr2;
+    id_list_node_curr->ptr2 = id_list_node_prev;
+    id_list_node_prev = id_list_node_curr;
+    id_list_node_curr = id_list_node;
+  }
+
+  /* pop the input parameters from the the stack */
+  size = 0;
+  id_list_node = module_reuse->ptr3;
+  while (id_list_node != NULL) {
+    size += 8;
+    id_list_node = id_list_node->ptr2;
+
+  }
+  
+  ic_addr.type = INTEGER;
+  ic_addr.value.num = size;
+  ic_addr.is_label = false;
+
+  ic_instr = newICInstruction();
+  ic_instr->addr1 = ic_addr;
+  ic_instr->op = icPOP_SHADOW;
+  global_ic_instr->next = ic_instr;
+  global_ic_instr = ic_instr;  
+}
+
+
 void icConditionalStatement(struct ConditionalStmtNode *conditional) {
   ICInstr *ic_instr = NULL;
   struct CaseStmtNode *case_statement = NULL;
@@ -831,7 +939,7 @@ void icStatement(struct Attribute *attribute_node) {
       return icAssignmentStatement(attribute_node->node.agn_stm);
       break;
     case MODULE_REUSE_STMT_NODE:
-      // return icModuleReuseStatement(attribute_node->node.mod_reu_stm);
+      return icModuleReuseStatement(attribute_node->node.mod_reu_stm);
       break;
     case INPUT_NODE:
       return icInputStatement(attribute_node->node.inp);
@@ -865,6 +973,55 @@ void icStatementList(struct StatementNode *statement_node) {
 }
 
 
+void icModule(struct ModuleNode *module_node) {
+  ICAddr ic_addr;
+  ICInstr *ic_instr;
+  struct ModuleEntry *symbol_entry;
+
+  /* populate address for the module label */
+  ic_addr.type = IDENTIFIER;
+  ic_addr.value.symbol = module_node->ptr1->value.entry;
+  ic_addr.is_label = true;
+
+  /* instruction: label for calling the module */
+  ic_instr = newICInstruction();
+  ic_instr->addr1 = ic_addr;
+  ic_instr->op = icLABEL;
+  global_ic_instr->next = ic_instr;
+  global_ic_instr = ic_instr;
+
+  /* populate address for the module entry instruction */
+  symbol_entry = resolveModule((char *) module_node->ptr1->value.entry);
+  ic_addr.value.num = symbol_entry->activation_record_size;
+  printf("%s %d", symbol_entry->name, symbol_entry->activation_record_size);
+  ic_addr.type = INTEGER;
+  ic_addr.is_label = false;
+
+  /* instruction: module entry instruction for set up */
+  ic_instr = newICInstruction();
+  ic_instr->addr1 = ic_addr;
+  ic_instr->op = icENTER;
+  global_ic_instr->next = ic_instr;
+  global_ic_instr = ic_instr;
+
+  icStatementList(module_node->ptr4);
+
+  /* instruction: return from module call */
+  ic_instr = newICInstruction();
+  ic_instr->op = icRET;
+  global_ic_instr->next = ic_instr;
+  global_ic_instr = ic_instr;
+}
+
+
+void icModuleList(struct OtherModuleNode *other_module_node) {
+  while (other_module_node != NULL) {
+    icModule(other_module_node->ptr1);
+    other_module_node = other_module_node->ptr2;
+  }
+}
+
+
 /**
  * This function is the entrypoint into generating intermediate code
  * for the source program. It can be called once the global AST and
@@ -873,22 +1030,38 @@ void icStatementList(struct StatementNode *statement_node) {
  * IntermediateCodeInstruction).
  */
 void generateIntermediateCode() {
+  ICAddr ic_addr;
+  ICInstr *ic_instr;
+
   global_ic_instr = newICInstruction();
   global_ic_instr->addr1 = newLabel(NULL);
   global_ic_instr->op = icLABEL;
   start_global_ic_instr = global_ic_instr;
+
   icStatementList(AST.ptr3);
-  // icModuleList(AST.ptr2);
-  // icModuleList(AST.ptr4);
+
+  ic_addr.value.symbol = (char *) malloc(sizeof(char) * 5);
+  strcpy((char *) ic_addr.value.symbol, "exit");
+  ic_addr.type = IDENTIFIER;
+  ic_addr.is_label = true;
+  
+  ic_instr = newICInstruction();
+  ic_instr->addr1 = ic_addr;
+  ic_instr->op = icJUMP;
+  global_ic_instr->next = ic_instr;
+  global_ic_instr = ic_instr;
+
+  icModuleList(AST.ptr2);
+  icModuleList(AST.ptr4);
 }
 
 
 void printICAddress(ICAddr ic_addr) {
   switch (ic_addr.type) {
-    case NUM:
+    case INTEGER:
       printf("%d\t", ic_addr.value.num);
       break;
-    case RNUM:
+    case REAL:
       printf("%f\t", ic_addr.value.rnum);
       break;
     case BOOLEAN_:
@@ -1036,9 +1209,33 @@ void printICInstruction(ICInstr *ic_instr) {
       printICAddress(ic_instr->addr1);
       printICAddress(ic_instr->addr2);
       break;
-    // case icCALL:
-    //   printf("CALL\t\t\t");
-    //   break;
+    case icCALL:
+      printf("CALL\t");
+      printICAddress(ic_instr->addr1);
+      break;
+    case icENTER:
+      printf("ENTER\t");
+      printICAddress(ic_instr->addr1);
+      break;
+    case icRET:
+      printf("RET\t");
+      break;
+    case icPUSH:
+      printf("PUSH\t");
+      printICAddress(ic_instr->addr1);
+      break;
+    case icPOP:
+      printf("POP\t");
+      printICAddress(ic_instr->addr1);
+      break;
+    case icPUSH_SHADOW:
+      printf("PUSH SHADOW\t");
+      printICAddress(ic_instr->addr1);
+      break;
+    case icPOP_SHADOW:
+      printf("POP SHADOW\t");
+      printICAddress(ic_instr->addr1);
+      break;
     case icARRAY:
       printf("ARRAY");
       printf("\t");
@@ -1049,6 +1246,7 @@ void printICInstruction(ICInstr *ic_instr) {
       printICAddress(ic_instr->addr1);
       break;
     default:
+      printf("%d", ic_instr->op);
       break;
   }
   printf("\n");

src/intCodeDef.h
@@ -16,22 +16,26 @@ typedef enum IntermediateCodeOperation {
   icINC, icDEC,
 
   /* logic */
-  icAND, icOR,
+  icAND, icOR, //12
 
   /* comparison */
-  icEQ, icNE, icLT, icGT, icLE, icGE,
+  icEQ, icNE, icLT, icGT, icLE, icGE, //18
 
   /* unary */
-  icPLUS, icMINUS,
+  icPLUS, icMINUS, //20
 
   /* data */
-  icMOV, icPRINT, icGET,
+  icMOV, icPRINT, icGET, //23
 
   /* control */
-  icJUMP, icJUMPZ, icJUMPNZ, icCALL,
+  icJUMP, icJUMPZ, icJUMPNZ, //26
 
   /* array */
-  icARRAY,
+  icARRAY, //27
+
+  /* module resuse */
+  icCALL, icENTER, icRET, // 30
+  icPUSH, icPOP, icPUSH_SHADOW, icPOP_SHADOW,
 
   /* others */
   icLABEL, icErrARRAY

commit fdb8915feafd172e72a8b5ebf7881af3f1875705

st: Fix broken widths.
Broken by commit 2071639b6b52352ceabca372b38850466e0714db.

src/codeGen.c
@@ -375,7 +375,7 @@ void cgICAddr(char *instr_list, char *addr, ICAddr *ic_addr) {
     symbol_entry = (struct VariableEntry *) ic_addr->value.array.var;
     
       base_offset = symbol_entry->mem_offset;
-      data_size = getDatatypeSize(symbol_entry->datatype);
+      data_size = 8;
 
     if (symbol_entry->isStatic) {
       lower_bound = symbol_entry->lower_bound->value.num;
@@ -804,7 +804,7 @@ void staticArrayUtil(char *instr_list, struct VariableEntry *symbol_entry) {
   base_offset = symbol_entry->mem_offset;
   lower_bound = symbol_entry->lower_bound->value.num;
   upper_bound = symbol_entry->upper_bound->value.num;
-  data_size = getDatatypeSize(symbol_entry->datatype);
+  data_size = 8;
 
   num_elements = upper_bound - lower_bound + 1;
   base_offset = base_offset + OFFSET * 3 + num_elements * data_size;
@@ -842,7 +842,7 @@ void dynamicArrayUtil(char *instr_list, struct VariableEntry *symbol_entry) {
   base_offset = symbol_entry->mem_offset;
   lower_bound_offset = symbol_entry->lower_bound->mem_offset;
   upper_bound_offset = symbol_entry->upper_bound->mem_offset;
-  data_size = getDatatypeSize(symbol_entry->datatype);
+  data_size = 8;
 
   /* load base address of array from array pointer */
   sprintf(tmp, "[rbp - %d]", base_offset + OFFSET);
@@ -1145,7 +1145,7 @@ void cgDynamicArrayAllocation(ICInstr *ic_instr) {
   symbol_entry = (struct VariableEntry *) ic_instr->addr1.value.symbol;
   lower_bound_offset = symbol_entry->lower_bound->mem_offset;
   upper_bound_offset = symbol_entry->upper_bound->mem_offset;
-  data_size = getDatatypeSize(symbol_entry->datatype);
+  data_size = 8;
 
   /*
     Compute total size of the array:

src/st.c
@@ -1113,17 +1113,14 @@ getDatatypeSize (enum terminal datatype)
     {
       case (NUM):
       case (INTEGER):
-        return DT_INTEGER_SIZE;
-        // return 2;
+        return 2;
       case (RNUM):
       case (REAL):
-        return DT_REAL_SIZE;
-        // return 4;
+        return 4;
       case (TRUE_):
       case (FALSE_):
       case (BOOLEAN_):
-        return DT_BOOL_SIZE;
-        // return 1;
+        return 1;
       default:
         fprintf(stderr, invalid_datatype_error_message, terminalStringRepresentations[datatype]);
         exit(EXIT_FAILURE);